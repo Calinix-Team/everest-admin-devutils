44b435e2189f5475f6bfd32af9eaac41e1b70f9e
Arghya Sarkar <arghyasarkar.nolan@gmail.com>
2022-01-26 16:09:39 +0530

    Initial COmmit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tools and Scripts for Admins of Everest Store
+
+These are some basic scripts I use as an Everest store admin to make work easier
+
+Run `install.sh` to get the scripts installed on your system
+
+- `getlocalpkg` - Search for package binaries and return available links (Never select *.sig files for Binary)
---
+++ install.sh
@@ -0,0 +1,19 @@
+#     _    ____  
+#    / \  / ___|   Arghya Sarkar
+#   / _ \ \___ \   https://github.com/arghyagod-coder
+#  / ___ \ ___) |  https://github.com/Calinix-Team
+# /_/   \_\____/   https://get.calinix.tech
+
+rootuser () {
+  if [[ "$EUID" = 0 ]]; then
+    continue
+  else
+    echo "Please Run As Root"
+    sleep 2
+    exit
+  fi
+}
+
+rootuser
+chmod +x bin/*
+cp +x bin/* /usr/local/bin/
